0826a41d03b0ad624d9d8400ac3344bb0d34cd62
arifweb2020 <arifhussainb2019@gmail.com>
2022-05-06 12:36:03 +0530

    progress bar integration

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -12,6 +12,58 @@ function About() {
 
     const [open, setOpen] = useState(false)
 
+    const [popup, setPopup] = useState(true)
+
+
+    const modal = true
+
+    let readings = [
+        {
+            name: 'Apples',
+            value: 60,
+            color: '#eb4d4b'
+        },
+        {
+            name: 'Blueberries',
+            value: 7,
+            color: '#22a6b3'
+        },
+        {
+            name: 'Guavas',
+            value: 23,
+            color: '#6ab04c'
+        },
+        {
+            name: 'Grapes',
+            value: 5,
+            color: '#e056fd'
+        }
+    ];
+
+    let values = readings.map((item, i) => {
+
+        if (item.value > 0) {
+            return (
+                <div className="value" style={{ 'color': item.color, 'width': item.value + '%' }} key={i}>
+                    <span>{item.value}%</span>
+                </div>
+            )
+        }
+    })
+
+    // eslint-disable-next-line array-callback-return
+    // let bars = readings.map((item, i) => {
+
+    //     if (item.value > 0) {
+    //         return (
+    //             // eslint-disable-next-line no-useless-concat
+    //             <div className="value" style={{ backgroundColor: "item.color", width: "item.value" + '%' }} key={i}>
+
+    //             </div>
+    //         )
+    //     }
+    // })
+
     return (
         <div className="aboutContainer">
             <div className="jumbotron text-center">
@@ -28,6 +80,98 @@ function About() {
                         </div>
                     )
                 }
+
+                {
+                    popup && modal ?
+                        <div style={{ width: '200px', height: '100px', background: 'purple', marginTop: '20px' }}>
+                            <span style={{ color: '#fff', cursor: 'pointer' }} onClick={() => setPopup(false)}>close</span>
+
+                        </div> : null
+
+                }
+
+
+
+                {/* {
+                    // eslint-disable-next-line array-callback-return
+                    readings.map((item, i) => {
+                        // return (
+                        //     <h1>{item.color}</h1>
+                        // )
+                        if (item.value > 0) {
+                            return (
+                                <div className="value" style={{ 'color': item.color, 'width': item.value + '%' }} key={i}>
+                                    <span>|</span>
+                                </div>
+                            )
+                        }
+                    })
+                } */}
+
+                <div className="multicolor-bar">
+                    <div className="values">
+                        {values}
+                    </div>
+                    <div className="scale">
+                        {readings.map((items, i) => {
+
+                            if (items.value > 0) {
+                                return (
+
+                                    <div className="graduation" style={{ 'color': items.color, 'width': items.value + '%' }} key={i}>
+                                        <span>|</span>
+                                    </div>
+                                )
+                            }
+                        })
+                        }
+                    </div>
+                    {/* <div className="bars" >
+                        {readings.map((items, i) => {
+
+                            if (items.value > 0) {
+                                return (
+
+                                    <div className="bar" style={{ 'backgroundColor': items.color, 'width': items.value + '%' }} key={i}>
+
+                                    </div>
+                                )
+                            }
+                        })
+                        }
+                    </div> */}
+                    <div className="progress" style={{ width: '100%' }}>
+                        {readings.map((items, i) => {
+
+                            if (items.value > 0) {
+                                return (
+
+                                    <div className="bar" style={{ 'backgroundColor': items.color, 'width': items.value + '%' }} key={i}>
+
+                                    </div>
+                                )
+                            }
+                        })
+                        }
+                    </div>
+                    <div className="legends">
+                        {readings.map((item, i) => {
+
+                            if (item.value > 0) {
+                                return (
+
+                                    <div className="legend" key={i}>
+                                        <span className="dot" style={{ 'color': item.color }}>●</span>
+                                        <span className="label">{item.name}</span>
+                                    </div>
+
+                                )
+                            }
+                        })
+                        }
+                    </div>
+                </div>
+
             </div>
             <div className="container">
                 <div className="row mt-5">
